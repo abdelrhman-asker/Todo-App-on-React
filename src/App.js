49766a6ebe2dee1c23e5a03f3d60639ab691f9e4
unknown <abdosker11@yahoo.com>
2022-09-19 20:21:00 +0200

    todo-app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react'
+import Home from './Home'
+import 'animate.css';
+import "./App.css"
 
-function App() {
+import { ClimbingBoxLoader } from 'react-spinners';
+// import TodoForm from './components/TodoForm';
+
+
+const App = () => {
+  const [loading , setLoading] = useState(false);
+
+    useEffect(() =>{
+        setLoading(true)
+        setTimeout(() =>{
+            setLoading(false)
+
+        } , 3000)
+
+    },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    { loading ? (<ClimbingBoxLoader className="animate__animated animate__flipInX" style={{display:"flex", justifyContent:"center", alignItems:"center", color:"brown", position:"fixed", top:"50%", left:"50%"} } loading={loading}/>) : ( <Home />)
+}    
+    </>
+   
+  )
 }
 
-export default App;
+export default App
---
+++ src/Home.js
@@ -0,0 +1,15 @@
+import 'animate.css';
+import TodoList from './components/TodoList';
+const Home = () => {
+    
+  return (
+    <div>
+      
+
+    <h1 className="animate__animated animate__bounce" ><TodoList />
+</h1>
+</div>
+  )
+}
+
+export default Home
